f21c01fc65117a0be60a1adc13fc60eb82493ffc
樊勇 <fanyong109@163.com>
2024-03-21 10:18:17 +0800

    python基础代码上传

---
+++ 17_tools/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding = utf-8 -*-
+# @Time       : 2023/6/14 15:06
+# @Author     : fany
+# @Project    : PyCharm
+# @File       : test.py
+# @description:
+i = 9
+while True:
+    if i == 9:
+        break
